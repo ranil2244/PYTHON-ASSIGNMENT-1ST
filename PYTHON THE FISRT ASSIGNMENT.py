ebfa63e888408ecbfe1e92a7d8b8d632e1945369
RANIL K,R <ranilkr77@gmail.com>
2024-10-05 16:14:12 +0530

    Add files via upload

---
+++ PYTHON THE FISRT ASSIGNMENT.py
@@ -0,0 +1,41 @@
+# print ("Ranil ST1007 ranil77@gmail.com")
+#
+# print("Ranil\nST1007\nranil77@gmail.com")
+#
+# a=10
+# b=15
+# print(a+b)
+# print(a-b)
+# print(a/b)
+# print(a*b)
+#
+# for i in range(1,6):
+#     print(i)
+# #
+# print('"SDK" stands for "Software Development Kit", whereas "IDE" stands for "Integrated Development Environment"')
+# #
+# print("python is an \"awsome\"language")
+# print("python\n\t2023")
+# print('I\'m from Entri.\b')
+# print("\65")
+# print("\x65")
+# print("Entri\n2023")
+# print("Entri\b2023")
+# print("Entri*2023",end="\b\b\b\b")
+#
+# print("\n")
+#
+# num =23
+# textnum ="57"
+# decimal =98.3
+# total =num +int(textnum)+ decimal
+# print(type(total))
+#
+# days =365
+# hours =24
+# mins = 60
+# total = days*hours*mins
+# print(total)
+# # #
+# l=input("Enter your name")
+# print(f"Hi {l},Welcome to the python programming")
